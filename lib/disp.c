91d50e6361c039c7e1f3c1d74b53d445f2d854f1
Михаил <dltech174@gmail.com>
2019-12-04 23:08:42 +0700

    ---

--- lib/disp.c
+++ lib/disp.c
@@ -5,14 +5,17 @@ void sdInit()
                            (uint8_t)
     }
     // сперва интерфейс
+    i2c_set_7bit_addr_mode(DISPI2C);
+    i2c_set_speed(DISPI2C,i2c_speed_fm_400k,SYSCLKHZ/1e6);
     // todo: blocking send seq
 //             dma send seq
 //             обработчик по окончанию (так же структура состояние отслеживать придется или по регистрам только удастся если да то круто)
 }
 
-void blockingSengSeq()
+void blockingSengSeq(uint8_t *seq)
 {
-    
+    i2c_send_start(DISPI2C);
+    i2c_send_data()
 }
 
 void fromDotMatrixToData()
--- sch_doc/aydongle/aydongle.pro
+++ sch_doc/aydongle/aydongle.pro
@@ -1,4 +1,4 @@
-update=Сб 30 ноя 2019 22:04:40
+update=Вс 01 дек 2019 12:55:26
 version=1
 last_client=kicad
 [general]
@@ -40,12 +40,14 @@ MinMicroViaDrill=0.09999999999999999
 MinHoleToHole=0.25
 TrackWidth1=0.25
 TrackWidth2=0.3
-TrackWidth3=0.4
-TrackWidth4=0.5
-TrackWidth5=1
-TrackWidth6=1.5
-TrackWidth7=2
-TrackWidth8=5
+TrackWidth3=0.35
+TrackWidth4=0.4
+TrackWidth5=0.5
+TrackWidth6=0.6
+TrackWidth7=1
+TrackWidth8=1.5
+TrackWidth9=2
+TrackWidth10=5
 ViaDiameter1=0.8
 ViaDrill1=0.4
 dPairWidth1=0.2
